53651f53a4985d32944fe14d2fc813ff0d527dbd
Evgen <zhenyalezhenin@gmail.com>
2023-09-18 21:33:46 +0300

    changed styling method

--- src/components/atoms/howItsWork/howItsWork.jsx
+++ src/components/atoms/howItsWork/howItsWork.jsx
@@ -4,14 +4,13 @@ import image1 from "./../../../assets/img/page3/photo1.png";
 import image2 from "./../../../assets/img/page3/photo2.png";
 import image3 from "./../../../assets/img/page3/photo3.png";
 import image4 from "./../../../assets/img/page3/photo4.png";
+import clsx from "clsx";
 
 const HowItWorkDesk = ({howItWorkListData}) => {
-  
-
   return (
     <ul className={styles.howItsWorkList}>
       {howItWorkListData.map((item) => (
-        <li key={item.id} className={styles.howItsWorkItem}>
+        <li key={item.id} className={clsx(styles.howItsWorkItem, item.classNames)}>
           <div className={styles.howItsWorkImgContainer}>
             <div className={styles.greenBorder}></div>
             <div className={styles.imgNumber}>{item.id}</div>
